d2bdb3049b47aff562e29062bdfe88fa865d5561
Jason Cruz <jasoncruz1986@gmail.com>
2026-05-10 22:26:18 -0500

    Add project files.

---
+++ IdleGame.Backend/Models/GeneratorState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdleGame.Backend.Models
+{
+    internal class GeneratorState
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public double StoredAmount { get; set; } = 0;
+        public double CurrentProductionRate { get; set; }
+        public double CurrentUpgradeCost { get; set; }
+        public DateTime LastCollected { get; set; }
+    }
+}
