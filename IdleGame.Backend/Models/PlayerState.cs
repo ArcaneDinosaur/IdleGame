e152a8361486ce75385984db8a1f6e70e6a7462e
Jason S Cruz <jasoncruz1986@gmail.com>
2026-05-11 00:31:11 -0500

    WIP state classes

---
+++ IdleGame.Backend/Models/PlayerState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdleGame.Backend.Models
+{
+    internal class PlayerState
+    {
+        public List<ResourceState> Resources { get; set; } = new List<ResourceState>();
+        public List<GeneratorState> Generators { get; set; } = new List<GeneratorState>();
+        public ArcaneDustGeneratorState? ArcaneDustGenerator { get; set; } = new ArcaneDustGeneratorState();
+        public List<Unlocks> Unlocks { get; set; } = new List<Unlocks>();
+        public List<Crafts> Crafts { get; set; } = new List<Crafts>();
+    }
+}
