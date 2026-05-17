712f9997a9fa1328c3004ed986054b5a881003ee
Jason Cruz <jasoncruz1986@gmail.com>
2026-05-17 00:05:11 -0500

    WIP generator definition

--- IdleGame.Backend/Definitions/GeneratorDefinition.cs
+++ IdleGame.Backend/Definitions/GeneratorDefinition.cs
@@ -6,23 +6,23 @@
 
         public double BaseAmountPerCycle { get; init; }
 
-        public double AmountPerLevel { get; init; } = 0;
+        public double AmountPerLevel { get; init; }
 
         public int BaseUpgradeCost { get; init; }
 
-        public double UpgradeCostMultiplier { get; init; } = 1.12;
+        public double UpgradeCostMultiplier { get; init; }
 
         public string Description { get; init; }
 
         public double ProductionIntervalSeconds { get; init; }
 
-        public double ProductionMultiplier { get; init; } = 1.10;
+        public double ProductionMultiplier { get; init; }
 
         public double MaxStorageAmount { get; init; }
 
-        public double StoragePerLevel { get; init; } = 0;
+        public double StoragePerLevel { get; init; }
 
-        public double StorageMultiplier { get; init; } = 1.08;
+        public double StorageMultiplier { get; init; }
 
         public Guid ProducesResourceId { get; init; }
 
